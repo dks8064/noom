30a2be13ab7fdce011444e24041119a102e5ac8b
Yewon Ahn <dks8064@naver.com>
2023-09-21 23:24:03 +0900

    :sparkles: feat: peer video using WebRTC

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,7 @@
 const socket = io();
 
+const call = document.getElementById("call");
+
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
@@ -8,6 +10,10 @@ const camerasSelect = document.getElementById("cameras");
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+call.hidden = true;
 
 async function getCameras() {
     try{
@@ -52,9 +58,7 @@ async function getMedia(deviceId) {
     }
 }
 
-getMedia();
-
-function handleMuteClink() {
+function handleMuteClick() {
     myStream
         .getAudioTracks()
         .forEach((track) => track.enabled = !track.enabled);
@@ -67,7 +71,7 @@ function handleMuteClink() {
     muted = !muted;
 }
 
-function handleCameraClink() {
+function handleCameraClick() {
     myStream
         .getVideoTracks()
         .forEach((track) => track.enabled = !track.enabled);
@@ -82,8 +86,84 @@ function handleCameraClink() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if(myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+            .getSenders()
+            .find((sender) => sender.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
-muteBtn.addEventListener("click", handleMuteClink);
-cameraBtn.addEventListener("click", handleCameraClink);
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+
+// Welcome
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+
+// Socket
+
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", async (offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+});
+
+socket.on("answer", (answer) => {
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+    myPeerConnection.addICECandidate(ice);
+});
+
+
+// WebRTC
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream
+        .getTracks()
+        .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
