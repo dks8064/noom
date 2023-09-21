48f017b6955b8808e3769097af8e57af12530604
Yewon Ahn <dks8064@naver.com>
2023-09-21 21:22:07 +0900

    :sparkles: feat: user video

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import SocketIO from "socket.io";
+import http from "http";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+});
+
+httpServer.listen(3000, console.log(`Listening on ws://localhost:3000`));
